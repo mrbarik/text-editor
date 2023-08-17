3deeee0817e5ecfa311aa848d4e21d09e36f0f8e
Md. A. Barik <115731019+mrbarik@users.noreply.github.com>
2023-08-17 12:23:13 +0600

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,15 +1,1 @@
 
-function addClassById(id, className) {
-    const textField = document.getElementById(id);
-    textField.classList.toggle(className);
-}
-
-function addClassByIdToInput(id, className) {
-    const textField = document.getElementById(id);
-    if (textField.classList.contains('text-right')) textField.classList.remove('text-right');
-    if (textField.classList.contains('text-center')) textField.classList.remove('text-center');
-    if (!textField.classList.contains("text-left"))
-        textField.classList.add("text-left");
-    else
-        textField.classList.remove("text-left");
-}
